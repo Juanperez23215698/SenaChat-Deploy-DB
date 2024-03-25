158405b7c1fbfda6ed81dd766ffc65f92f6fef42
JUAN CARDENAS PEREZ <114235553+Juanperez23215698@users.noreply.github.com>
2024-03-25 14:43:23 -0500

    Update conexion.js

--- src/controladores/conexion.js
+++ src/controladores/conexion.js
@@ -1,12 +1,22 @@
 const mysql = require("mysql2");
 
+
+const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_USER = process.env.DB_USER || "root";
+const DB_PASSWORD = process.env.DB_PASSWORD || "";
+const DB_DATABASE = process.env.DB_DATABASE || "sena_chat";
+const DB_PORT = process.env.DB_PORT || 3306;
+
 const conexion = mysql.createConnection({
-  host: process.env.HOST || "localhost",
-  database: process.env.DB || "sena_chat",
-  user: process.env.USER || "root",
-  password: process.env.PASSWORD || "",
-  port: process.env.PORT || 3306,
+  host: DB_HOST,
+  database: DB_DATABASE,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  port: DB_PORT,
 });
+
+
+
 
 conexion.connect((error) => {
   if (error) throw error;
